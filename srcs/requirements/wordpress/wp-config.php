a74fe4b835b7e0993a3b90f245d769bacb55fa5e
yssefnoam <2020noam@gmail.com>
2021-05-29 16:19:06 +0100

    hh

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'ynoam' );
 define( 'DB_PASSWORD', 'ynoam' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mariadb');
+define( 'DB_HOST', 'mariadb:3306');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
