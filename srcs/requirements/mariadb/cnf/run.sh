64c92690b70b4cc164b436ee3fc25a7506065a30
Youssef Noam <ynoam@e1r11p6.1337.ma>
2021-05-31 11:45:44 +0100

    test

--- srcs/requirements/mariadb/cnf/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-service mariadb start
-mysql -e "create database wordpress;"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'ynoam'@'%' IDENTIFIED BY 'ynoam';"
-#e&& mysql wordpress < /wordpress.sql
