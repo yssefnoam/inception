955da04f7e13a78fb1ed02f08c00de85d104475e
Youssef Noam <ynoam@e1r12p10.1337.ma>
2021-12-16 18:54:51 +0100

    a

--- srcs/requirements/mariadb/cnf/setup.sh
+++ srcs/requirements/mariadb/cnf/setup.sh
@@ -1,11 +1,7 @@
-# mysqld --initialize-insecure --datadir=/var/lib/mysql
 
-service mysql start
-mysql -e "create database wordpress;" 
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' IDENTIFIED BY '$PASS';" 
-mysql wordpress < /wordpress.sql
-rm -fr /wordpress.sql
-rm -fr /etc/mysql/my.cnf
 chown -R mysql:mysql /var/lib/mysql
+service mysql start
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' IDENTIFIED BY '$PASS';"
+mysql wordpress < /wordpress.sql
+service mysql stop
 mysqld_safe
-# tail -f /dev/null
