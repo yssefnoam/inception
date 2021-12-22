c0b4d1f1b51f17c0faf653f044e1b0816da400a3
Youssef Noam <ynoam@e1r8p2.1337.ma>
2021-12-22 03:52:23 +0100

    A

--- srcs/requirements/mariadb/cnf/setup.sh
+++ srcs/requirements/mariadb/cnf/setup.sh
@@ -2,9 +2,9 @@
 chown -R mysql:mysql /var/lib/mysql
 if [ -z "$(ls -A /var/lib/mysql/wordpress)" ]; then
     service mysql start
+    mysql wordpress < /wordpress.sql
     mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
-    mysql wordpress < /wordpress.sql
     service mysql stop
 fi
 mysqld_safe
