c774998e1fc9ebfa0e9d9cf523b908209e032733
yssefnoam <2020noam@gmail.com>
2022-02-03 18:33:56 +0100

    a

--- srcs/requirements/wordpress/cnf/setup.sh
+++ srcs/requirements/wordpress/cnf/setup.sh
@@ -8,14 +8,11 @@ define('WP_HOME',    'https://localhost');
 PHP
 
 while true; do
-    mysqladmin -h mariadb -u $DB_USER -p$DB_ROOT_PASS ping
+    /wp --allow-root core install --url="https://$DOMAIN_NAME" --title=inception --admin_user=$WP_USER1 --admin_password=$WP_USER1_PASS --admin_email="$WP_USER1@inception.com" && /wp --allow-root user  create  $WP_USER2 $WP_USER2@inception.com  --user_pass=$WP_USER2_PASS
     if [ $? -eq 0 ]; then
         break
     fi
     sleep 1
 done
-
-/wp --allow-root core install --url="https://$DOMAIN_NAME" --title=inception --admin_user=$WP_USER1 --admin_password=$WP_USER1_PASS --admin_email="$WP_USER1@inception.com"
-/wp --allow-root user  create  $WP_USER2 $WP_USER2@inception.com  --user_pass=$WP_USER2_PASS
 
 php-fpm7.3 -F
