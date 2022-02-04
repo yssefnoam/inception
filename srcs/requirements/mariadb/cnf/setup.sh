0a2605eed69b41139da291685b06eccd727573aa
Youssef Noam <36737715+yssefnoam@users.noreply.github.com>
2022-02-04 22:59:49 +0100

    Update setup.sh

--- srcs/requirements/mariadb/cnf/setup.sh
+++ srcs/requirements/mariadb/cnf/setup.sh
@@ -1,7 +1,9 @@
 chown -R mysql:mysql /var/lib/mysql
 
-service mysql start 
-mysqladmin -u root password $DB_ROOT_PASS
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" 
-service mysql stop
-mysqld_safe 
+if [ -z "$(ls -A /var/lib/mysql/wordpress)" ]; then
+    service mysql start 
+    mysqladmin -u root password $DB_ROOT_PASS
+    mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" 
+    service mysql stop
+fi
+mysqld_safe --skip-grant-tables # start mysql server without root password
